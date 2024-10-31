01b7bd298a250bd94730691e8f58320472a07877
Steffano99 <amosasenso@gmail.com>
2024-10-31 07:03:36 -0700

    initialization of a text to speech application

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:demo_app/pages/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen(
+    key: UniqueKey(),
+    onInitializationComplete: () {},
+  ));
+}
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashScreen({super.key, required this.onInitializationComplete});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+       title: 'TalkIt',
+       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+       ),
+       home: Scaffold(
+        body: Center(
+          child:Container(
+            height: 250,
+          width: 250,
+          decoration:const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/TalkIt.png')),
+          ),
+          )
+
+        ),
+       ),
+    );
+  }
+}
